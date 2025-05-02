25e7c5bbe24a0ce2509d643e6aac0efaf9c5d29a
Bar Kobi <122670126+barkobi40@users.noreply.github.com>
2025-05-02 14:28:22 +0300

    Updated ProfileViewModel

--- app/src/main/java/com/example/roomatchapp/presentation/roommate/ProfileViewModel.kt
+++ app/src/main/java/com/example/roomatchapp/presentation/roommate/ProfileViewModel.kt
@@ -1,3 +1,24 @@
 package com.example.roomatchapp.presentation.roommate
 
-data class ProfileViewModel()
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.roomatchapp.data.local.dao.RoommatesDao
+import com.example.roomatchapp.data.model.Roommate
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class ProfileViewModel(
+    private val roommatesDao: RoommatesDao
+) : ViewModel() {
+
+    private val _roommate = MutableStateFlow<Roommate?>(null)
+    val roommate: StateFlow<Roommate?> = _roommate.asStateFlow()
+
+    fun loadRoommateById(id: Int) {
+        viewModelScope.launch {
+            _roommate.value = roommatesDao.getById(id)
+        }
+    }
+}
